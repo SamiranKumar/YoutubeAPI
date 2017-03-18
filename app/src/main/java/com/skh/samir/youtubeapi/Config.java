ee8bf8c39337fcf5e0fdcff4e854b2a8124ca1d8
samiran <samirankumar11@gmail.com>
2017-03-18 18:50:11 +0600

    Initial commit

---
+++ app/src/main/java/com/skh/samir/youtubeapi/Config.java
@@ -0,0 +1,12 @@
+package com.skh.samir.youtubeapi;
+
+/**
+ * Created by samir on 3/18/2017.
+ */
+
+public class Config {
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCG25IwSEZcJuF5Te7kko9XawkHaEJ48Ws";
+}
---
+++ app/src/main/java/com/skh/samir/youtubeapi/YouTubePlayerView/MainActivity.java
@@ -0,0 +1,42 @@
+package com.skh.samir.youtubeapi.YouTubePlayerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.skh.samir.youtubeapi.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+
+        findViewById(R.id.basic_player_example).setOnClickListener(this);
+        findViewById(R.id.minimal_player_example).setOnClickListener(this);
+        findViewById(R.id.custom_player_control_example).setOnClickListener(this);
+        findViewById(R.id.player_fragment_example).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.basic_player_example:
+                startActivity(new Intent(this, BasicPlayerActivity.class));
+                break;
+            case R.id.minimal_player_example:
+                startActivity(new Intent(this, MinimalPlayerActivity.class));
+                break;
+            case R.id.custom_player_control_example:
+                startActivity(new Intent(this, CustomPlayerControlActivity.class));
+                break;
+            case R.id.player_fragment_example:
+                startActivity(new Intent(this, YouTubePlayerFragmentActivity.class));
+                break;
+
+        }
+    }
+}
